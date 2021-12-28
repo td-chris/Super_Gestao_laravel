2dbb095cc75181f9e243eea27a3e15969d75a5b4
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-12-28 14:27:32 -0300

    Feat: create new routes

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -38,8 +38,32 @@ Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'sobre
 
 Route::get('/contatos', [\App\Http\Controllers\ContatosController::class,'contatos']);
 
+Route::get('/login', function() { return "login"; } );
+
+Route::get('/clientes', function() { return "clientes"; } );
+
+Route::get('/fornecedores', function() { return "fornecedores"; } );
+
+Route::get('/produtos', function() { return "produtos"; } );
 
 
+// Rotas passando parametros
+
+// Route::get('/contatos/{nome}/{categoria}/{assunto}/{mensagem?}', // a interrogação indica um parametro opcional
+//     function(string $nome, string $categoria, string $assunto, string $mensagem = "mensagem não indicada"){
+//         echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+// });
+
+// Route::get('/contatos/{nome}/{categoria_id}', 
+//     function(
+//         string $nome = "Desconhecido",
+//         int $categoria_id = 1,
+//         ) {
+//            echo "Estamos aqui: $nome - $categoria_id";
+//         }
+//     )->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+'); 
+        // categoria_id só pode ser um numero de 0 a 9 e precisa ter pelo menos 1 numero (+)
+        // nome so pode receber caracteres de A-Z ou a-z e precisa ter pelo menos 1 caracter (+)
 
 // verbos http:
 
